fef4a1bf7c65d3a0f42f802e36a6c7cc4b89157b
Juan Gonzalez <jgu1@hotmail.com>
2022-01-29 01:32:34 -0300

    Fix to prettier formatting

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import {
-	FastifyAdapter,
-	NestFastifyApplication,
+  FastifyAdapter,
+  NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestFastifyApplication>(
-		AppModule,
-		new FastifyAdapter()
-	);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
 
-	const port = process.env.PORT || 8000;
-	await app.listen(port, '0.0.0.0');
+  const port = process.env.PORT || 8000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
